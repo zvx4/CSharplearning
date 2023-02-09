a6f274fa70db9578425abc46b60d2b0a79ee589c
朱思源 <zvxzvx4@gmail.com>
2023-02-09 23:04:04 +0800

    6.15 20230209

---
+++ 6.15.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Class1
+{
+	public void ListInts(params int[] inVals )
+	{
+        if ((inVals != null) && (inVals.Length != 0))
+            for (int i=0; i<inVals.Length; i++)
+            {
+				inVals[i] = inVals[i] * 10;
+				Console.WriteLine($"{inVals[i]}");
+            }
+	}
+}
+
+class Program
+{
+	static void Main()
+
+    {
+		int first = 5, second = 6, third = 7;
+
+		Class1 c1= new();
+		c1.ListInts(first, second ,third);
+
+		Console.WriteLine($"{first},{second},{third}");
+    }
+}
